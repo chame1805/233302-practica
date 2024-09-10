fb6d025d2a6f485431b78d8bf7beae49be714737
chame <angelchame6@gmail.com>
2024-09-10 13:31:02 -0600

    primer commit

---
+++ src/app/users/users.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersDashboardComponent } from './users-dashboard/users-dashboard.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    UsersDashboardComponent,
+    UserFormComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+
+  exports : [
+    UsersDashboardComponent
+  ]
+})
+export class UsersModule { }
